89d7b69d855afe338dbe17978d703fcfadbaafd0
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2023-01-12 14:51:25 +0100

    add changes

---
+++ utils/tests/cutText.test.js
@@ -0,0 +1,12 @@
+const cutText = require('../cutText.js');
+const expect = require('chai').expect;
+
+describe('CutText', () => {
+
+  it('should throw an error if "content" arg is not a string', () => {
+    expect(() => { cutText(undefined, 20) }).to.throw('Error');
+    expect(() => { cutText(12, 20) }).to.throw('Error');
+    expect(() => { cutText({}, 20) }).to.throw('Error');
+    expect(() => { cutText([], 20) }).to.throw('Error');
+    expect(() => { cutText(function () { }, 20) }).to.throw('Error');
+  });
